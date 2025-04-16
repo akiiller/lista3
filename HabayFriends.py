6242eba628d749f26a66411dc75ef3ddde99c4c9
adriano <admkiiller@gmail.com>
2025-04-16 16:25:46 -0300

    Amigo do Habay 5% errada no beecrowd commit

--- HabayFriends.py
+++ HabayFriends.py
@@ -1,6 +1,7 @@
 habayFriend = ""
 signedList = []
 signedYes = []
+signedNo = []
 s = ""
 
 while s != "FIM":
@@ -13,13 +14,18 @@ for s in signedList:
         signedYes.append(s)
         if len(habayFriend) < len(s):
             habayFriend = s
+    elif "NO" in s:
+        signedNo.append(s)
 
 print('Saída')
 ms = set(signedYes)
 signedYes = list(ms)
 signedYes.sort()
+signedNo.sort()
+signedList.clear()
+signedList = signedYes + signedNo
 
-for y in signedYes:
-    print(y[:y.index("YES")])
+for y in signedList:
+    print(y[:y.index("YES"  if "YES" in y else "NO")])
 
 print("Amigo do Habay: \n", habayFriend[:habayFriend.index("YES")])
